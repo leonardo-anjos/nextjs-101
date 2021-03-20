202ca19c6069a875f8efa03570ae8d2cc8b349df
Leonardo Anjos <leodrums11@gmail.com>
2021-03-20 18:44:53 -0300

    Assets, Metadata and CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,16 @@
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <section className={utilStyles.headingMd}>
+        <p>Software Engennier | Computer Scientist | Drummer</p>
+      </section>
+    </Layout>
   )
 }
